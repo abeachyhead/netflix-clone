11bdc528802bd31fbb67adc820a04a7f4f82dd1a
abeachyhead <amybeachyhead@gmail.com>
2022-01-25 20:49:35 +0200

    rendered movie posters

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
@@ -14,14 +17,24 @@ function Row({ title, fetchUrl }) {
     fetchData();
   }, [fetchUrl]);
 
-  console.log(movies);
+  // console.log(movies);
 
   return (
-    <div>
+    <div className="row">
       {/* Title*/}
       <h2>{title}</h2>
 
       {/* Container -> thumbnails */}
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
