ac037629b4970318970bbeb988c9a5f8019384d3
abeachyhead <amybeachyhead@gmail.com>
2022-01-26 06:53:44 +0200

    added nav bar

--- src/Nav.js
+++ src/Nav.js
@@ -1,4 +1,36 @@
 import React, { useState, useEffect } from "react";
-import axios from "./axios";
-import requests from "./requests";
 import "./Nav.css";
+
+function Nav() {
+  // nav bar transition
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+      />
+
+      <img
+        className="nav__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="User Profile Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
